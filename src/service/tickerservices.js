f47dbdf6177896ebeae77f3c222b1f0dda9b02c7
plus-tech <63340325+plus-tech@users.noreply.github.com>
2024-04-21 15:24:05 +0900

    Update tickerservices.js

--- src/service/tickerservices.js
+++ src/service/tickerservices.js
@@ -11,28 +11,16 @@ export function getTickerList(fmt='DICTIONARY') {
   /**
    * Get the list of symbols from Nasdaq.
    * 
-   * @param {String} fmt : indicates the format of the prices
+   * @param {String} fmt : indicates the format of the symbols
    * @returns : A list of symbols
    */
   const endurl = getRestApiUrl();
 
   const path = endurl + '/symbollist';
 
-  axios.get(path, {
+  return axios.get(path, {
     params: {
       fmt: fmt
     }
-  })
-  .then(function (response) {
-    return response;
-  })
-  .catch(function (error) {
-    console.log(error);
-    // 
-    // throw error to the external layer
-    throw error;
-  })
-  .finally(function () {
-    // add if any
   });
 }
